859a828d9bc31a4dd2f398a248e047952385e2bd
sushaz9 <susannahelisabeth1@gmail.com>
2023-10-23 14:13:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <p>This is working</p>
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Profile.jsx
@@ -0,0 +1,5 @@
+function Profile() {
+  return <p>Tim is best.</p>;
+}
+
+export default Profile;
